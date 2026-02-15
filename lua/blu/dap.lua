2b90f11d0592e7b3a243047322f24e7df7619da0
blu@bluArch <menditta1@gmail.com>
2026-02-15 12:38:51 +0000

    add go dap config

--- lua/blu/dap.lua
+++ lua/blu/dap.lua
@@ -5,6 +5,27 @@ dap.adapters.lldb = {
   name = 'lldb'
 }
 
+dap.adapters.delve = function (callback, config)
+  if config.mode =='remote' and config.request =='attach' then
+    callback({
+      type = 'server',
+      host = config.host or '127.0.0.1',
+      port = config.port or '38697'
+    })
+  else
+    callback({
+      type = 'server',
+      port = '${port}',
+      executable = {
+        command = 'dlv',
+        args = {'dap', '-l', '127.0.0.1:${port}', '--log', '--log-output=dap'},
+        detached = vim.fn.has("win32") == 0
+      }
+    })
+  end
+end
+
+
 dap.configurations.cpp = {
   {
     name = 'Launch',
@@ -22,3 +43,27 @@ dap.configurations.cpp = {
 dap.configurations.c = dap.configurations.cpp
 dap.configurations.zig = dap.configurations.cpp
 dap.configurations.rust = dap.configurations.cpp
+
+
+dap.configurations.go = {
+  {
+    name = "Debug",
+    type = 'delve',
+    request = "launch",
+    program = "${file}"
+  },
+  {
+    type = "delve",
+    name = "Debug test",
+    request = "launch",
+    mode = "test",
+    program = "${file}"
+  },
+  {
+    type = "delve",
+    name = "Debug test (go.mod)",
+    request = "launch",
+    mode = "test",
+    program = "./${relativeFileDirname}"
+  }
+}
